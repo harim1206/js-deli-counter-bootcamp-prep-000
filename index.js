94c132497f1f7d1b95996b8ad4500eb90a685b6f
Harim Choi <harim1206@gmail.com>
2018-01-30 15:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+function takeANumber(katzDeliLine, custName){
 
+  katzDeliLine.push(custName)
 
-function takeANumber(lineArr, custName){
-
-  lineArr.push(custName)
-
-  return `Welcome, ${custName}. You are number ${lineArr.length} in line.`
+  return `Welcome, ${custName}. You are number ${katzDeliLine.length} in line.`
 
 }
 
@@ -14,12 +12,10 @@ function nowServing(lineArr){
   if(lineArr.length === 0){
     return "There is nobody waiting to be served!"
   }else{
-    //save first customer in line in variable cust
-    var nextCust = lineArr[0]
-    //remove nextCust from line
+    var newCust = lineArr[0]
     lineArr.shift()
 
-    return `Currently serving ${nextCust}.`
+    return `Currently serving ${newCust}.`
   }
 
 }
@@ -28,11 +24,11 @@ function currentLine(lineArr){
 
   var str = ""
 
-  if(lineArr.length === 0){
+  if(lineArr.length===0){
     return `The line is currently empty.`
   }else{
     for(var i=0; i<lineArr.length; i++){
-      if(i === lineArr.length-1){
+      if(i===lineArr.length-1){
           str+= `${i+1}. ${lineArr[i]}`
       }else{
         str+= `${i+1}. ${lineArr[i]}, `
