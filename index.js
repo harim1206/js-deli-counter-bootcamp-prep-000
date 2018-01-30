86d80e1ca6607c027032e2ec45c5a406b53a849e
Harim Choi <harim1206@gmail.com>
2018-01-30 15:52:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,18 +34,21 @@ function currentLine(lineArr){
 
   var str = ""
 
+
   if(lineArr.length===0){
 
     return `The line is currently empty.`
   }else{
     for(var i=0; i<lineArr.length; i++){
       if(i===lineArr.length-1){
-          str+= `${i+1}. ${lineArr[i]}`
+          str+= `${lineArr[i]}`
       }else{
-        str+= `${i+1}. ${lineArr[i]}, `
+        str+= `${lineArr[i]}, `
       }
     }
 
     return `The line is currently: ${str}`
+    //The line is currently 4, 5, 6
+    // 1. 4, 2. 5, 3. 6
   }
 }
